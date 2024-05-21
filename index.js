602fd4c347def75797f54e20d2ef39dcd69eb413
ignacio <karmican@gmail.com>
2024-05-20 23:08:13 -0300

    agregar manejo de rutas en deploy

--- index.js
+++ index.js
@@ -26,7 +26,10 @@ app.use( express.json() );
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/events', require('./routes/events'));
 
-// TODO: CRUD eventos calendario
+// Deployment
+app.get('*', (req, res) => {
+    res.sendFile( __dirname + '/public/index.html');
+});
 
 // Escuchar peticiones
 
